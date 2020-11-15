8a19db74b16ce746a6e8b909625dafcdbda034ea
ehmusman <ehmusman@gmail.com>
2020-11-15 21:28:19 +0500

    counter is ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-
+import Counter from './components/Counter'
+import Toggler from './components/Toggler'
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [day, setDay] = useState(true)
   return (
-    <div className="App">
+    <div className="App" style={{ height: '100vh', backgroundColor: 'yellow' }}>
+      <Toggler
 
+      />
+      <button className="btn btn-primary btn-lg mt-5 ml-5" onClick={() => setCount(count + 1)}>
+        Click Me
+      </button>
+      <Counter
+        count={count}
+      />
     </div>
   );
 }
---
+++ src/components/Counter.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Counter({ count }) {
+    return (
+        <div>
+            <p className="h4 ml-5">You clicked {count} times</p>
+
+        </div>
+    )
+}

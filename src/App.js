7552e1fd24e03b5c21c6de0e6e64faf87a143002
ehmusman <ehmusman@gmail.com>
2020-11-15 22:07:19 +0500

    padding added

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       >
         Toggle Day
       </button>
-      <h3 className="pl-5">This is {isMorning ? "Day" : "Dark"} Mode</h3>
+      <h3 className="pl-5 pt-5">This is {isMorning ? "Day" : "Dark"} Mode</h3>
       <br />
       <button className="btn btn-primary btn-lg mt-5 ml-5" onClick={() => setCount(count + 1)}>
         Click Me

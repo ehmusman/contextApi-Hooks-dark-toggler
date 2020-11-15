60b390b9aed8e7dd6c28fb59f0929cdb238eccb2
ehmusman <ehmusman@gmail.com>
2020-11-15 22:06:33 +0500

    Dark and Day mode is also done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, { useState } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
 
 import Counter from './components/Counter'
-import Toggler from './components/Toggler'
+
 function App() {
   const [count, setCount] = useState(0);
-  const [day, setDay] = useState(true)
-  return (
-    <div className="App" style={{ height: '100vh', backgroundColor: 'yellow' }}>
-      <Toggler
+  const [isMorning, setMorning] = useState(true);
 
-      />
+
+  return (
+    <div className={isMorning ? "day" : "night"}>
+      <button
+        className="btn btn-dark btn-lg mt-5 ml-5"
+        onClick={() => setMorning(!isMorning)}
+      >
+        Toggle Day
+      </button>
+      <h3 className="pl-5">This is {isMorning ? "Day" : "Dark"} Mode</h3>
+      <br />
       <button className="btn btn-primary btn-lg mt-5 ml-5" onClick={() => setCount(count + 1)}>
         Click Me
       </button>
--- src/components/Toggler.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Toggler() {
-    return (
-        <h2 className="pt-5 pl-5">
-            This is Dark Mode
-        </h2>
-    )
-}
